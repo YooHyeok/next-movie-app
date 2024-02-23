8fda2d8569f6b5dbd1a057ee916e233653afae02
YooHyeok <yjk7454@naver.com>
2024-02-23 22:51:44 +0900

    feat: Navigation 샘플 컴포넌트 추가 및 Link Route 적용

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
-export default function Tomato() {
-  return <h1>Hello !</h1>
+import Navigation from "../components/navigation";
+
+export default function Home() {
+
+  return <div>
+    <Navigation />
+    <h1>Hello!</h1>
+  </div>
 }
