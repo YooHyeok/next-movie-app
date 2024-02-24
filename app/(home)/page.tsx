4cb92dd48939871c5f334e164b8e0b0693fafdc5
YooHyeok <yjk7454@naver.com>
2024-02-24 17:52:23 +0900

    feat: Client Side Fetch 예제 구현
    Client Side Fetch시 Client Component 의미인 use client를 명시해야하며 metadata를 export할 수 없다.

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,14 +1,19 @@
-
-/**
- * 페이지의 <head>영역에 출력될 정보
- */
-export const metadata = {
-  title: 'Home',
-}
+"use client" // Client Side Fetch시 Client Component 의미인 use client를 명시해야하며 metadata를 export할 수 없다.
+import { useEffect, useState } from "react";
 
 export default function Home() {
-
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies")
+    const json = await response.json()
+    setMovies(json);
+    setIsLoading(false)
+  }
+  useEffect(()=> {
+    getMovies();
+  }, [])
   return <div>
-    <h1>Hello!</h1>
+    {isLoading ? "Loading..." : JSON.stringify(movies)}
   </div>
 }
