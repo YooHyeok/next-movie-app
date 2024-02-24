0f89e8a2224c59cadb802d4b0785063b34e20c81
YooHyeok <yjk7454@naver.com>
2024-02-24 18:53:47 +0900

    feat: Server Side Fetch 에제 구현

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,19 +1,27 @@
-"use client" // Client Side Fetch시 Client Component 의미인 use client를 명시해야하며 metadata를 export할 수 없다.
-import { useEffect, useState } from "react";
+export const metadata = {
+  title: "Home",
+}
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies"
 
-export default function Home() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const response = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies")
-    const json = await response.json()
-    setMovies(json);
-    setIsLoading(false)
-  }
-  useEffect(()=> {
-    getMovies();
-  }, [])
+/**
+ * Servier Side Fetching
+ * Server Component는 서버에서 렌더링 된다.
+ * Server Component에서 데이터를 Fetch했기 때문에 네트워크 혹은 console을 찍어봐도 
+ * 클라이언트 측에서 데이터는 출력되지 않으므로 보안적인 측면에서 좋다.
+ * 새로고침 하더라도 fetch에 대한 응답이 캐싱되었기 때문에 데이터 로딩이 매우 빠르다.
+ * 최신데이터 관련 캐싱 or revalidation
+ * @returns 
+ */
+async function getMovies() {
+  await new Promise((resolve)=> setTimeout(resolve, 1000))
+  const response = await fetch(URL)
+  const json = await response.json()
+  return json;
+}
+
+export default async function HomePage() {
+  const movies = await getMovies();
   return <div>
-    {isLoading ? "Loading..." : JSON.stringify(movies)}
+    {JSON.stringify(movies)}
   </div>
 }
