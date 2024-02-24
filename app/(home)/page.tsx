74ff02fa623e25d32b23562084a91e39d17b94de
YooHyeok <yjk7454@naver.com>
2024-02-25 00:09:39 +0900

    fix: Home 영화목록 map 콜백 매개변수 타입용 영화 인터페이스 추가 및 적용

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -16,14 +16,17 @@ export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies"
  */
 async function getMovies() {
   // await new Promise((resolve)=> setTimeout(resolve, 1000))
-  const response = await fetch(URL)
+  const response = await fetch(API_URL)
   const json = response.json()
   return json;
 }
-
+interface IMovie {
+  id: string;
+  title: string;
+}
 export default async function HomePage() { //async: await되는동안 loading 컴포넌트 출력을 위한 서버컴포넌트 동기처리
   const movies = await getMovies();
   return <div>
-    {movies.map(movie=><li key={movie.id}><Link href={`/movies/${movie.id}`}>{movie.title}</Link></li>)}
+    {movies.map((movie:IMovie) =><li key={movie.id}><Link href={`/movies/${movie.id}`}>{movie.title}</Link></li>)}
   </div>
 }
