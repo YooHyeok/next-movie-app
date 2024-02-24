3f15cb1cefca48bb263334bb4900bf4c5304a31a
YooHyeok <yjk7454@naver.com>
2024-02-24 13:37:38 +0900

    feat: Metadata 중첩 적용

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,3 +1,11 @@
+
+/**
+ * 페이지의 <head>영역에 출력될 정보
+ */
+export const metadata = {
+  title: 'Home',
+}
+
 export default function Home() {
 
   return <div>
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,8 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: 'Not Founddddd',
+}
 export default function NotFound() {
   return <h1>Not Found!</h1>
 }
