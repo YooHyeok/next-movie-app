0ea8975227f4501be864065613e2444eefc8fd07
webdevyoo <yjk7454@naver.com>
2024-02-26 00:06:50 +0900

    design: Movie 출력용 클라이언트 컴포넌트 추가 및 모듈CSS 적용

---
+++ components/movie.tsx
@@ -0,0 +1,15 @@
+"use client" /* useRouter와 onClick event에 대한 Hydration을 위해 클라이언트 컴포넌트로 전환 */
+import Link from "next/link";
+import { IMovie } from "../app/(home)/page";
+import styles from "../styles/movie.module.css"
+import { useRouter } from "next/navigation";
+export default function Movie({id, poster_path, title}: IMovie) {
+  const router = useRouter();
+  const onClick = () => {
+    router.push(`/movies/${id}`)
+  }
+  return <div className={styles.movie} key={id}>
+  <img src={poster_path} alt={title} onClick={onClick}/>
+  <Link href={`/movies/${id}`}>{title}</Link>
+</div>
+}
